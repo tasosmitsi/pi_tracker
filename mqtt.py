d1f946ede3437faae75fa7b702297f92e8037b13
tasosmitsi <tasos1109@gmail.com>
2024-08-15 12:18:05 +0200

    The following callback functions have been added to mqtt client + on_connect + on_subscribe + on_unsubscribe

--- mqtt.py
+++ mqtt.py
@@ -1,4 +1,5 @@
 import paho.mqtt.client as mqtt
+from paho.mqtt.enums import MQTTProtocolVersion
 import fnmatch
 from actions import *
 
@@ -19,38 +20,16 @@ def mqtt_ini():
     client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2,
                          client_id=client_id,
                          transport="websockets",
-                         protocol=mqtt.MQTTv5)
-    
-    # Set the on_message callback function
-    client.on_message = on_message
+                         protocol=MQTTProtocolVersion.MQTTv5)
     
     # Connect to the MQTT broker using WebSockets
     client.ws_set_options(path=path)  # Set the WebSocket path
-
-    while True:
-        # Connect to the MQTT broker
-        try:
-            client.connect(broker, port, 60)
-            break
-        except Exception as e:
-            print(e)
-# ------------------------
-    while True:
-        # Subscribe to debuging topic
-        try:
-            client.subscribe(client_id + "/command/#")
-            break
-        except Exception as e:
-            print(e)
-# ------------------------
-    # for topic in subscribe_topics:
-    #     while True:
-    #         # Subscribe to the topic
-    #         try:
-    #             client.subscribe(topic)
-    #             break
-    #         except Exception as e:
-    #             print(e)
+    
+    # Set the on_message callback function
+    client.on_connect = on_connect
+    client.on_subscribe = on_subscribe
+    client.on_unsubscribe = on_unsubscribe
+    client.on_message = on_message
 
     while True:
         # Start the MQTT client loop in a separate thread
@@ -59,7 +38,15 @@ def mqtt_ini():
             break
         except Exception as e:
             print(e)
-            
+
+    while True:
+        # Connect to the MQTT broker
+        try:
+            client.connect(broker, port, 60)
+            break
+        except Exception as e:
+            print(e)
+
     return client
 
 
@@ -76,7 +63,7 @@ def on_message(client, userdata, msg):
     # do something to filter incoming messages and call the necessary functions
     # Decode the message payload
     message = msg.payload.decode()
-    print(f"Received message: {message} from topic: {msg.topic}")
+    print(f"Received message: {message} from topic: {msg.topic}, with qos: {msg.qos}")
     
     # Filter incoming messages based on the topic
     if msg.topic not in subscribe_topics:
@@ -100,8 +87,44 @@ def on_message(client, userdata, msg):
     # clear the errors by posting empty string
     post_message(config.mqtt_client, 'errors', f"")
     return
-        
 
+def on_connect(client, userdata, flags, reason_code, properties):
+    if reason_code.is_failure:
+        print(f"Failed to connect: {reason_code}. loop_forever() will retry connection")
+    else:
+        # we should always subscribe from on_connect callback to be sure
+        # our subscribed is persisted across reconnections.
+        # ------------------------
+        while True:
+            # Subscribe to debuging topic
+            try:
+                client.subscribe(client_id + "/command/#")
+                break
+            except Exception as e:
+                print(e)
+        # ------------------------
+        # for topic in subscribe_topics:
+        #     while True:
+        #         # Subscribe to the topic
+        #         try:
+        #             client.subscribe(topic)
+        #             break
+        #         except Exception as e:
+        #             print(e)
 
+def on_subscribe(client, userdata, mid, reason_code_list, properties):
+    # Since we subscribed only for a single channel, reason_code_list contains
+    # a single entry
+    if reason_code_list[0].is_failure:
+        print(f"Broker rejected you subscription: {reason_code_list[0]}")
+    else:
+        print(f"Broker granted the following QoS: {reason_code_list[0].value}")
 
-
+def on_unsubscribe(client, userdata, mid, reason_code_list, properties):
+    # Be careful, the reason_code_list is only present in MQTTv5.
+    # In MQTTv3 it will always be empty
+    if len(reason_code_list) == 0 or not reason_code_list[0].is_failure:
+        print("unsubscribe succeeded (if SUBACK is received in MQTTv3 it success)")
+    else:
+        print(f"Broker replied with failure: {reason_code_list[0]}")
+    # client.disconnect()
